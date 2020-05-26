a8dbfb7d23cede91128d7635c2b613ad17c74b9f
SMakaiTakori <makait87@icloud.com>
2020-05-26 14:14:40 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -11,5 +11,21 @@ module ArtistsHelper
     end        
 end
 
+# Official Solution
+# Display dropdown of artists, with name if editing through nested
 
+# def artist_select(song, path)
+#   if song.artist && path == "nested"
+#     hidden_field_tag "song[artist_id]", song.artist_id
+#   else
+#     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+#   end
+# end
+
+# Display name if editing through nested
+# def display_name(song, path)
+#   if song.artist && path == "nested"
+#     song.artist.name
+#   end
+# end
 end
